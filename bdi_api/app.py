6a3903678b7101d9ce57ec5bb8fb81ebbadf8ac1
Waldemar <waldepfeifer@gmail.com>
2025-03-16 11:46:00 +0100

    Updated app.py to access S7

--- bdi_api/app.py
+++ bdi_api/app.py
@@ -14,6 +14,7 @@ from bdi_api.examples import v0_router
 # from bdi_api.s1.exercise import s1
 from bdi_api.s1.exercise import s1
 from bdi_api.s4.exercise import s4
+from bdi_api.s7.exercise import s7
 from bdi_api.settings import Settings
 
 logger = logging.getLogger("uvicorn.error")
@@ -65,7 +66,7 @@ if settings.telemetry:
 app.include_router(v0_router)
 app.include_router(s1)
 app.include_router(s4)
-
+app.include_router(s7)
 
 @app.get("/health", status_code=200)
 async def get_health() -> JSONResponse:
